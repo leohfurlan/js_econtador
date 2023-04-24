5d1c85bb2e0819d63343993999ba72a28cca9d01
leohfurlan <furlanhleo@gmail.com>
2023-04-24 15:45:08 -0300

    Novo update settings.py

--- jse_contador/settings.py
+++ jse_contador/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-*i*ahvfk9og1i91mga))$ytqpza^ngsgan4mv#56i6al^(1j)s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
